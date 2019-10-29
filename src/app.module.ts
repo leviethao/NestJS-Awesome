9bfe2821431149c23c3a9ca07b3be4811f7b87a7
leviethao <leviethao2@gmail.com>
2019-10-29 12:28:02 +0700

    * add cat service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExampleModule } from './example/example.module';
+import { CatsService } from './cats/cats.service';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CatsService],
   imports: [ExampleModule],
 })
 export class AppModule {}
